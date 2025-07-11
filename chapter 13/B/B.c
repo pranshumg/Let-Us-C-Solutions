46fabb7ad41d60cb263f8f05caf6fa9486e554a5
Pranshu Gupta <pranshumg@gmail.com>
2025-07-11 13:16:08 +0530

    commit

---
+++ chapter 13/B/B.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+    int a[n];
+    printf("Enter %d integers: ", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &a[i]);
+    }
+    bool is_reverse = true;
+    int start = 0, end = n - 1;
+    while (start < end) {
+        if (a[start++] != a[end--]) {
+            is_reverse = false;
+            break;
+        }
+    }
+    if (is_reverse) {
+        printf("Array is a palindrome.\n");
+    } else {
+        printf("Array is not a palindrome.\n");
+    }
+    return 0;
+}
