af2b67fe2d81dc025db710df5c7209d17784330e
Pranshu Gupta <pranshumg@gmail.com>
2025-07-23 20:56:58 +0530

    commit

---
+++ chapter 16/A/b.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+bool is_vowel(char c) {
+    c = tolower(c);
+    return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'; 
+}
+
+int main() {
+    char s[81], result[81];
+    printf("Enter a sentence: ");
+    scanf("%[^\n]s", s);
+    int j = 0;
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (!is_vowel(s[i])) {
+            result[j++] = s[i];
+        }
+    }
+    result[j] = '\0';
+    printf("Sentence without vowels: %s\n", result);
+    return 0;
+}
