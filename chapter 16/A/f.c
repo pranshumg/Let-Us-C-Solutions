2e3059e08f278b5f0be0c5edb8030a061647d794
Pranshu Gupta <pranshumg@gmail.com>
2025-07-23 22:10:57 +0530

    commit

---
+++ chapter 16/A/f.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void print_word(char c) {
+    switch (c) {
+        case '0':
+        printf("Zero ");
+        break;
+        case '1':
+        printf("One ");
+        break;
+        case '2':
+        printf("Two ");
+        break;
+        case '3':
+        printf("Three ");
+        break;
+        case '4':
+        printf("Four ");
+        break;
+        case '5':
+        printf("Five ");
+        break;
+        case '6':
+        printf("Six ");
+        break;
+        case '7':
+        printf("Seven ");
+        break;
+        case '8':
+        printf("Eight ");
+        break;
+        case '9':
+        printf("Nine ");
+        break;
+    }
+}
+
+int main() {
+    char s[10];
+    printf("Enter number (<= 9 digits): ");
+    fgets(s, 10, stdin);
+    for (int i = 0; s[i] != '\0'; i++) {
+        print_word(s[i]);
+    }
+    printf("\n");
+    return 0;
+}
