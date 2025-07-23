f28fb92876c2fe4193cd217a1a95d9a97357b417
Pranshu Gupta <pranshumg@gmail.com>
2025-07-23 21:17:30 +0530

    commit

--- chapter 16/A/c.c
+++ chapter 16/A/c.c
@@ -6,7 +6,7 @@ int main() {
     char s[201], word[201];
     printf("Enter a sentence: ");
     fgets(s, 201, stdin);
-    int i = 0, j = 0, k = 0;
+    int i = 0, k = 0;
     while (s[i]) {
         if (isspace(s[i]) || s[i] == '\0') {
             word[k] = '\0';
