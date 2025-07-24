ca63b0036697dfdd48b356dc7b231a13d5cd07a4
Pranshu Gupta <pranshumg@gmail.com>
2025-07-24 17:06:21 +0530

    commit

---
+++ chapter 17/B/g.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+struct Date {
+    int day, month, year;
+};
+
+int compare_dates(struct Date d1, struct Date d2) {
+    return (d1.day == d2.day && d1.month == d2.month && d1.year == d2.year) ? 0 : 1;
+}
