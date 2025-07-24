835a27a9194e6a757f6fb975965af99b649c4050
Pranshu Gupta <pranshumg@gmail.com>
2025-07-24 15:31:40 +0530

    commit

---
+++ chapter 17/B/c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+struct part {
+    char serial_no[10];
+    int year;
+    char material[30];
+    int quantity;
+};
+
+void print_part_range(struct part p[], int n) {
+    for (int i = 0; i < n; i++) {
+        if (strcmp(p[i].serial_no, "BB1") >= 0 && strcmp(p[i].serial_no, "CC6") <= 0) {
+            printf("%s %d %s %d\n", p[i].serial_no, p[i].year, p[i].material, p[i].quantity);
+        }
+    }
+}
