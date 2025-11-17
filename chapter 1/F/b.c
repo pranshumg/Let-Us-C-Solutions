88c371416f2fa047d9be01683485b20f76272149
Pranshu Gupta <pranshumg@gmail.com>
2025-11-17 18:25:52 +0530

    area and perimeter of a rectangle, and the area and circumference of a circle

--- chapter 1/F/b.c
+++ chapter 1/F/b.c
@@ -6,15 +6,11 @@ int main() {
 	scanf("%f", &l);
 	printf("enter breadth of rectangle: ");
 	scanf("%f", &b);
-
 	printf("area of rectangle: %.2f\n", l * b);
 	printf("perimeter of rectangle: %.2f\n", 2 * (l + b));
-
 	printf("\nenter radius of circle: ");
 	scanf("%f", &r);
-
 	printf("area of circle: %.2f\n", 3.14 * r * r);
 	printf("circumference of circle: %.2f\n", 2 * 3.14 * r);
-
 	return 0;
 }
