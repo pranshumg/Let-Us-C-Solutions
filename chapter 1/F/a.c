de86902577d0eabaaae09bd87b64cb25e691e95b
Pranshu Gupta <pranshumg@gmail.com>
2025-11-17 18:24:40 +0530

    converts temp from f to c

--- chapter 1/F/a.c
+++ chapter 1/F/a.c
@@ -4,10 +4,7 @@ int main() {
 	float f, c; 
 	printf("enter temperature in fahrenheit: ");
 	scanf("%f", &f);
-
 	c = ((f - 32) * 5) / 9;
-
 	printf("%f into centigrade degree is: %.2f\n", f, c);
-
 	return 0;
 }
