b91decae761487dfe65de43a4083f779e64827b8
Pranshu Gupta <pranshumg@gmail.com>
2025-11-18 10:15:17 +0530

    calculates and prints the sum of the digits of a five-digit number

--- chapter 2/G/a.c
+++ chapter 2/G/a.c
@@ -1,18 +1,15 @@
 #include <stdio.h>
 
 int main() {
-    int n;
-    printf("Enter a five-digit number: ");
-    scanf("%d", &n);
-
-    int sum = 0, og_n = n;
-    while (n != 0) {
-        int d = n % 10;
-        sum += d;
-        n /= 10;
-    }
-
-    printf("Sum of digits of %d: %d\n", og_n, sum);
-
-    return 0;
+  int n;
+  printf("Enter a five-digit number: ");
+  scanf("%d", &n);
+  int sum = 0, og_n = n;
+  while (n != 0) {
+    int d = n % 10;
+    sum += d;
+    n /= 10;
+  }
+  printf("Sum of digits of %d: %d\n", og_n, sum);
+  return 0;
 }
