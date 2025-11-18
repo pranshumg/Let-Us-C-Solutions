d4fa1ac47b29805c5756287d7d4378605381e7af
Pranshu Gupta <pranshumg@gmail.com>
2025-11-18 10:21:31 +0530

    changed format specifier

--- chapter 2/G/e.c
+++ chapter 2/G/e.c
@@ -6,11 +6,11 @@ int main() {
 	printf("enter value of an angle in degree: ");
 	scanf("%lf", &d);
 	r = d * (pi / 180);
-	printf("sin(%.2f) = %.2lf\n", d, sin(r));
-	printf("cos(%.2f) = %.2lf\n", d, cos(r));
-	printf("tan(%.2f) = %.2lf\n", d, tan(r));
-	printf("cosec(%.2f) = %.2lf\n", d, 1 / sin(r));
-	printf("sec(%.2f) = %.2lf\n", d, 1 / cos(r));
-	printf("cot(%.2f) = %.2lf\n", d, 1 / tan(r));
+	printf("sin(%.2lf) = %.2lf\n", d, sin(r));
+	printf("cos(%.2lf) = %.2lf\n", d, cos(r));
+	printf("tan(%.2lf) = %.2lf\n", d, tan(r));
+	printf("cosec(%.2lf) = %.2lf\n", d, 1 / sin(r));
+	printf("sec(%.2lf) = %.2lf\n", d, 1 / cos(r));
+	printf("cot(%.2lf) = %.2lf\n", d, 1 / tan(r));
 	return 0;
 }
