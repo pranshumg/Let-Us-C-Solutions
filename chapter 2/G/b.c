c79230334510d411273855f6946b0e462b8e9d0a
Pranshu Gupta <pranshumg@gmail.com>
2025-11-18 10:16:57 +0530

    convert Cartesian coordinates entered by the user into their corresponding Polar coordinates

--- chapter 2/G/b.c
+++ chapter 2/G/b.c
@@ -7,14 +7,10 @@ int main() {
 	scanf("%d", &x);
 	printf("enter y co-ordinate: ");
 	scanf("%d", &y);
-
 	float r = sqrt(x * x + y * y);
 	float phi = atan(y / x);
-
 	printf("polar co-ordinates of (%d,%d) is: (%.2f,%.2f)", x, y, r, phi);
-
 	float pi = 3.1415, degree = phi * (180 / pi);
 	printf("\npolar co-ordinates of (%d,%d) in degree is: (%.2f,%.2f)", x, y, r, degree);
-
 	return 0;
 }
