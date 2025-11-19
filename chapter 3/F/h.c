ed81c80eafac9c07427bf567f0989028c4fee57c
Pranshu Gupta <pranshumg@gmail.com>
2025-11-19 15:02:27 +0530

    determines the location of a point (x, y) on a 2D coordinate plane, specifically checking if it lies on the X-axis, Y-axis, at the Origin, or in a Quadrant

--- chapter 3/F/h.c
+++ chapter 3/F/h.c
@@ -1,17 +1,19 @@
 #include <stdio.h>
 
 int main() {
-    int x, y;
-    printf("enter x co-ordinate of point: ");
-    scanf("%d", &x);
-    printf("enter y co-ordinate of point: ");
-    scanf("%d", &y);
-
-    if (x == 0) {
-        printf("the point lies on y-axis.\n");
-    } else {
-        printf("the point lies on x-axis.\n");
-    }
-    
-    return 0;
+  int x, y;
+  printf("enter x co-ordinate of point: ");
+  scanf("%d", &x);
+  printf("enter y co-ordinate of point: ");
+  scanf("%d", &y);
+  if (x == 0 && y == 0) {
+    printf("the point is the origin (0, 0).\n");
+  } else if (x == 0) {
+    printf("the point lies on the y-axis.\n");
+  } else if (y == 0) {
+    printf("the point lies on the x-axis.\n");
+  } else {
+    printf("the point lies in a quadrant.\n");
+  }
+  return 0;
 }
