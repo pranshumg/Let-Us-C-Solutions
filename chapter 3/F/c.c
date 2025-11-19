af928444e5286a6f82f6208a9d63cfb498ede291
Pranshu Gupta <pranshumg@gmail.com>
2025-11-19 14:57:05 +0530

    checks whether three given angles can form a valid triangle by verifying if their sum equals 180 degrees

--- chapter 3/F/c.c
+++ chapter 3/F/c.c
@@ -8,15 +8,12 @@ int main() {
 	scanf("%f", &a2);
 	printf("enter 3rd angle of triangle: ");
 	scanf("%f", &a3);
-
 	float sum = a1 + a2 + a3;
 	printf("sum of angles: %.2f\n", sum);
-
 	if (sum == 180) {
 		printf("triangle is valid\n");
 	} else {
 		printf("triangle is not valid\n");
 	}
-	
 	return 0;
 }
