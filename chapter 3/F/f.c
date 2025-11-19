85b69352812f0cd10a1dce481d1511074b8e6dbc
Pranshu Gupta <pranshumg@gmail.com>
2025-11-19 14:59:52 +0530

    determines whether three given points in a 2D coordinate system are collinear

--- chapter 3/F/f.c
+++ chapter 3/F/f.c
@@ -14,12 +14,10 @@ int main() {
 	scanf("%f", &x3);
 	printf("enter y co-ordinate of 3rd point: ");
 	scanf("%f", &y3);
-
 	if ((y2 - y1) / (x2 - x1) == (y3 - y2) / (x3 - x2)) {
 		printf("all the three points fall on one straight line.\n");
 	} else {
 		printf("all the three points does not fall on one straight line.\n");
 	}
-
 	return 0;
 }
