c1eb476106994a7f8a18c6db03bca4233e0eb73c
Pranshu Gupta <pranshumg@gmail.com>
2025-11-19 14:55:10 +0530

    check if a five-digit number entered by the user is a palindrome

--- chapter 3/F/a.c
+++ chapter 3/F/a.c
@@ -5,21 +5,17 @@ int main() {
 	printf("enter a five-digit number: ");
 	scanf("%d", &num);
 	int og_num = num;
-
 	int rev_num = 0;
 	while (num > 0) {
 		int d = num % 10;
 		rev_num = rev_num * 10 + d;
 		num /= 10;
 	}
-
 	printf("reversed number: %d\n", rev_num);
-	
 	if (rev_num == og_num) {
 		printf("original and reversed numbers are equal.\n");
 	} else {
 		printf("original and reversed numbers are not equal.\n");
 	}
-
 	return 0;
 }
