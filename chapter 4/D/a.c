24f6cd08e917a3f66134a37d3757d03d3a187c7b
Pranshu Gupta <pranshumg@gmail.com>
2025-11-20 19:23:55 +0530

    prompts the user to enter the lengths of the three sides of a triangle and then determines and prints its type based on those lengths

--- chapter 4/D/a.c
+++ chapter 4/D/a.c
@@ -8,30 +8,25 @@ int main() {
 	scanf("%d", &b);
 	printf("enter 3rd side of triangle: ");
 	scanf("%d", &c);
-
 	if (a == b && b == c) {
 		printf("triangle is equilateral.\n");
 	} else {
 		printf("triangle is not equilateral.\n");
 	}
-
 	if (a == b || b == c || c == a) {
 		printf("triangle is isosceles.\n");
 	} else {
 		printf("triangle is not isosceles.\n");
 	}
-
 	if (a != b && b != c && c != a) {
 		printf("triangle is scalene.\n");
 	} else {
 		printf("triangle is not scalene.\n");
 	}
-
 	if ((a * a + b * b == c * c) || (b * b + c * c == a * a) || (c * c + a * a == b * b)) {
 		printf("triangle is an right-angled triangle.\n");
 	} else {
 		printf("triangle is not an right-angled triangle.\n");
 	}
-
 	return 0;
 }
