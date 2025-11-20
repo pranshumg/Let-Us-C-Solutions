d9c98c8c9720c943b0771905bb611cdee27e71e7
Pranshu Gupta <pranshumg@gmail.com>
2025-11-20 19:29:11 +0530

    checks if a single character entered by the user is a lowercase alphabet and prints the result

--- chapter 4/E/a-1.c
+++ chapter 4/E/a-1.c
@@ -4,8 +4,6 @@ int main() {
 	char ch;
 	printf("enter character: ");
 	scanf("%c", &ch);
-
 	ch >= 97 && ch <= 122 ? printf("%c is a lowercase alphabet\n", ch) : printf("%c is not a lowercase alphabet\n", ch);
-
 	return 0;
 }
