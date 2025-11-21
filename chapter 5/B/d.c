4477b5212ba42d1de8fbea6db71c5386e2f0f78a
Pranshu Gupta <pranshumg@gmail.com>
2025-11-21 17:58:05 +0530

    a simple looping counter that allows a user to repeatedly enter numbers and then tallies how many of those numbers were positive, negative, or zero

--- chapter 5/B/d.c
+++ chapter 5/B/d.c
@@ -3,11 +3,9 @@
 int main() {
 	int n, pos = 0, neg = 0, z = 0; 
 	char ch;
-	
 	do {
 		printf("enter a number: ");
 		scanf("%d", &n);
-
 		if (n > 0) {
 			pos++;
 		} else if (n < 0) {
@@ -15,13 +13,9 @@ int main() {
 		} else {
 			z++;
 		}
-
 		printf("do you still want to continue (Y / N): ");
 		scanf(" %c", &ch);
-
 	} while (ch == 'Y' || ch == 'y');
-
 	printf("postive numbers: %d\nnegative numbers: %d\nzeros: %d\n", pos, neg, z);
-
 	return 0;
 }
