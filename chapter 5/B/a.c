cd8b09b8f2d32676c054008cb1d598685a63966f
Pranshu Gupta <pranshumg@gmail.com>
2025-11-21 17:55:44 +0530

    prints out the ASCII characters and their corresponding decimal values (from 0 to 255)

--- chapter 5/B/a.c
+++ chapter 5/B/a.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 int main() {
-	char ch = (char)0;
+	char c = (char)0;
 	int n = 0;
-
 	while (n <= 255) {
-		printf("%c - %d\n", ch, n);
-		ch++;
+		printf("%c - %d\n", c, n);
+		c++;
 		n++;
 	}
-	
 	return 0;
 }
