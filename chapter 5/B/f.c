a08102753b266935a31246b5d75a761d9159e158
Pranshu Gupta <pranshumg@gmail.com>
2025-11-21 17:59:23 +0530

    calculates the range of a set of integers provided by the user, which is the difference between the maximum and minimum values entered

--- chapter 5/B/f.c
+++ chapter 5/B/f.c
@@ -4,17 +4,13 @@ int main() {
 	int n, nums, max, min;
 	printf("how many numbers you want to enter: ");
 	scanf("%d", &nums);
-
 	printf("enter number: ");
 	scanf("%d", &n);
-
 	max = min = n;
-
 	int i = 2;
 	while (i <= nums) {
 		printf("enter number: ");
 		scanf("%d", &n);
-
 		if (n > max) {
 			max = n;
 		} else if (n < min) {
@@ -22,9 +18,7 @@ int main() {
 		}
 		i++;
 	}
-
 	int range = max - min; 
 	printf("Range: %d - %d = %d\n", max, min, range);
-
 	return 0;
 }
