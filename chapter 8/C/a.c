a64fbf5d9f50c69a218f14d63905c0cf33713627
Pranshu Gupta <pranshumg@gmail.com>
2025-11-24 16:27:33 +0530

    determines whether a year entered by the user is a leap year or not

--- chapter 8/C/a.c
+++ chapter 8/C/a.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 
 void leap_year(int year) {
-    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-        printf("%d is a leap year\n", year);
-    } else {
-        printf("%d is not a leap year\n", year);
-    }
+  if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+    printf("%d is a leap year\n", year);
+  } else {
+    rintf("%d is not a leap year\n", year);
+  }
 }
 
 int main() {
-    int year;
-    printf("Enter year: ");
-    scanf("%d", &year);
-    leap_year(year);
-
-    return 0;
+  int year;
+  printf("Enter year: ");
+  scanf("%d", &year);
+  leap_year(year);
+  return 0;
 }
