4719793b706d6d0d0b3315be920fc5ce197e226d
Pranshu Gupta <pranshumg@gmail.com>
2025-11-26 17:27:49 +0530

    decimal-to-binary conversion

--- chapter 10/B/a-2.c
+++ chapter 10/B/a-2.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 
 void dec_to_bin(int *b, int n, int c) {
-    if (n == 0) {
-        return;
-    } else {
-        int rem = n % 2;
-        *b = *b + rem * c;
-        c *= 10;
-        n /= 2;
-        dec_to_bin(b, n, c);
-    }
+  if (n == 0) {
+    return;
+  } else {
+    int rem = n % 2;
+    *b = *b + rem * c;
+    c *= 10;
+    n /= 2;
+    dec_to_bin(b, n, c);
+  }
 }
 
 int main() {
-    int n, b = 0, c = 1;
-
-    printf("Enter a decimal number: ");
-    scanf("%d", &n);
-
-    dec_to_bin(&b, n, c);
-
-    printf("Binary equivalent of %d: %d\n", n, b);
-
-    return 0;
+  int n, b = 0, c = 1;
+  printf("Enter a decimal number: ");
+  scanf("%d", &n);
+  dec_to_bin(&b, n, c);
+  printf("Binary equivalent of %d: %d\n", n, b);
+  return 0;
 }
