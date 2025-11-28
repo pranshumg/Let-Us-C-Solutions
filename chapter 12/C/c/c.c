a0d2ac0bba1f46878a9cce3ca9383cc5a395815f
Pranshu Gupta <pranshumg@gmail.com>
2025-11-28 19:54:51 +0530

    calculates and displays the simple interest and the total amount based on user input

--- chapter 12/C/c/c.c
+++ chapter 12/C/c/c.c
@@ -2,10 +2,10 @@
 #include "interest.h"
 
 int main() {
-    float p, r, t;
-    printf("Enter Principal, Rate and Time: ");
-    scanf("%f %f %f", &p, &r, &t);
-    printf("Simple Interest: %f\n", simple_interest(p, r, t));
-    printf("Amount: %f\n", amount(p, r, t));
-    return 0;
+  float p, r, t;
+  printf("Enter Principal, Rate and Time: ");
+  scanf("%f %f %f", &p, &r, &t);
+  printf("Simple Interest: %f\n", simple_interest(p, r, t));
+  printf("Amount: %f\n", amount(p, r, t));
+  return 0;
 }
