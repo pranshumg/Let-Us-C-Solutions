fbab5e615d2962a87033ef4123254462b340fffd
Pranshu Gupta <pranshumg@gmail.com>
2025-11-30 20:52:42 +0530

    calculates the total path length by summing the distances between consecutive points entered by the user

--- chapter 13/B/i.c
+++ chapter 13/B/i.c
@@ -2,16 +2,16 @@
 #include <math.h>
 
 int main() {
-    float x[10], y[10];
-    printf("Enter coordinates (x, y) of 10 points: \n");
-    for (int i = 0; i < 10; i++) {
-        scanf("%f %f", &x[i], &y[i]);
-    }
-    float distance = 0;
-    for (int i = 1; i < 10; i++) {
-        float d_x = x[i] - x[i - 1], d_y = y[i] - y[i - 1];
-        distance += sqrt(d_x * d_x + d_y * d_y);
-    }
-    printf("Sum of distances between consecutive points: %f\n", distance);
-    return 0;
+  float x[10], y[10];
+  printf("Enter coordinates (x, y) of 10 points: \n");
+  for (int i = 0; i < 10; i++) {
+    scanf("%f %f", &x[i], &y[i]);
+  }
+  float distance = 0;
+  for (int i = 1; i < 10; i++) {
+    float d_x = x[i] - x[i - 1], d_y = y[i] - y[i - 1];
+    distance += sqrt(d_x * d_x + d_y * d_y);
+  }
+  printf("Sum of distances between consecutive points: %f\n", distance);
+  return 0;
 }
