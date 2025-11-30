c1f5592dc25a06c9e3db6dcd1f08616a7fa718c4
Pranshu Gupta <pranshumg@gmail.com>
2025-11-30 20:47:04 +0530

    checks if an array of integers is a palindrome

--- chapter 13/B/b.c
+++ chapter 13/B/b.c
@@ -2,26 +2,26 @@
 #include <stdbool.h>
 
 int main() {
-    int n;
-    printf("Enter size of array: ");
-    scanf("%d", &n);
-    int a[n];
-    printf("Enter %d integers: ", n);
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+  int n;
+  printf("Enter size of array: ");
+  scanf("%d", &n);
+  int a[n];
+  printf("Enter %d integers: ", n);
+  for (int i = 0; i < n; i++) {
+    scanf("%d", &a[i]);
+  }
+  bool is_reverse = true;
+  int start = 0, end = n - 1;
+  while (start < end) {
+    if (a[start++] != a[end--]) {
+      is_reverse = false;
+      break;
     }
-    bool is_reverse = true;
-    int start = 0, end = n - 1;
-    while (start < end) {
-        if (a[start++] != a[end--]) {
-            is_reverse = false;
-            break;
-        }
-    }
-    if (is_reverse) {
-        printf("Pattern matched.\n");
-    } else {
-        printf("Pattern not matched.\n");
-    }
-    return 0;
+  }
+  if (is_reverse) {
+    printf("Pattern matched.\n");
+  } else {
+    printf("Pattern not matched.\n");
+  }
+  return 0;
 }
