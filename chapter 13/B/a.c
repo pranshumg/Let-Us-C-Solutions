e43d5ff8468d6f8ee0cf22a2abbb3e22bf06f2f4
Pranshu Gupta <pranshumg@gmail.com>
2025-11-30 20:45:57 +0530

    ead 25 integers from the user and then count and display how many of those integers are positive, negative, even, and odd

--- chapter 13/B/a.c
+++ chapter 13/B/a.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int a[25], pos = 0, neg = 0, even = 0, odd = 0;
-    printf("Enter 25 integers: ");
-    for (int i = 0; i < 25; i++) {
-        scanf("%d", &a[i]);
-        if (a[i] > 0) {
-            pos++;
-        } else if (a[i] < 0) {
-            neg++;
-        }
-        if (a[i] % 2 == 0) {
-            even++;
-        } else {
-            odd++;
-        }
+  int a[25], pos = 0, neg = 0, even = 0, odd = 0;
+  printf("Enter 25 integers: ");
+  for (int i = 0; i < 25; i++) {
+    scanf("%d", &a[i]);
+    if (a[i] > 0) {
+      pos++;
+    } else if (a[i] < 0) {
+      neg++;
     }
-    printf("Postive: %d\nNegative: %d\nEven: %d\nOdd: %d\n", pos, neg, even, odd);
-    return 0;
+    if (a[i] % 2 == 0) {
+      even++;
+    } else {
+      odd++;
+    }
+  }
+  printf("Postive: %d\nNegative: %d\nEven: %d\nOdd: %d\n", pos, neg, even, odd);
+  return 0;
 }
