fac69824f1039d532ab7b42040155135b3a20e2a
Pranshu Gupta <pranshumg@gmail.com>
2025-11-30 20:50:53 +0530

    calculates the area of six different triangular plots and then determines which plot has the largest area

--- chapter 13/B/g.c
+++ chapter 13/B/g.c
@@ -2,18 +2,18 @@
 #include <math.h>
 
 int main() {
-    float a[] = {137.4, 155.2, 149.3, 160.0, 155.6, 149.7};
-    float b[] = {80.9, 92.62, 97.93, 100.25, 68.95, 120.0};
-    float angle[] = {0.78, 0.89, 1.35, 9.00, 1.25, 1.75};
-    float area, max_area = 0;
-    int max_plot = 0;
-    for (int i = 0; i < 6; i++) {
-        area = 0.5 * a[i] * b[i] * sin(angle[i]);
-        if (area > max_area) {
-            max_area = area;
-            max_plot = i + 1;
-        }
+  float a[] = {137.4, 155.2, 149.3, 160.0, 155.6, 149.7};
+  float b[] = {80.9, 92.62, 97.93, 100.25, 68.95, 120.0};
+  float angle[] = {0.78, 0.89, 1.35, 9.00, 1.25, 1.75};
+  float area, max_area = 0;
+  int max_plot = 0;
+  for (int i = 0; i < 6; i++) {
+    area = 0.5 * a[i] * b[i] * sin(angle[i]);
+    if (area > max_area) {
+      max_area = area;
+      max_plot = i + 1;
     }
-    printf("largest area is Plot no. %d with area %f\n", max_plot, max_area);
-    return 0;
+  }
+  printf("largest area is Plot no. %d with area %f\n", max_plot, max_area);
+  return 0;
 }
